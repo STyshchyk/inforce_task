366249a4412e81d0796d8adeae7a2823cd6aa2e6
STyshchyk <svet.tyshchyk@gmail.com>
2023-03-30 12:15:09 +0300

    feat: add store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
+import {createHashRouter, RouterProvider} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./store/store";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
 );
 
---
+++ src/store/itemApi.js
@@ -0,0 +1,44 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+export const itemApi = createApi({
+    reducerPath: "itemApi",
+    tagTypes: ["Products"],
+    baseQuery: fetchBaseQuery(
+        {
+            baseUrl: "http://localhost:3001/"
+        }),
+    endpoints: (build) => ({
+        getGoods: build.query({
+            query: (query = "") => ({
+                url: `${query}`
+            }),
+            providesTags: (result) => result
+                ? [
+                    ...result.map(({id}) => ({type: 'Products', id})),
+                    {type: 'Products', id: 'LIST'},
+                ]
+                : [{type: 'Products', id: 'LIST'}],
+
+        }),
+        addProduct: build.mutation({
+            query: (body) => ({
+                url: `product`,
+                method: "POST",
+                body
+            }),
+            invalidatesTags: [{type: 'Products', id: 'LIST'}],
+        }),
+        deleteProduct: build.mutation({
+            query: (id) => ({
+                url: `product/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: [{type: 'Products', id: 'LIST'}],
+        })
+    }),
+
+
+})
+
+export const {useGetGoodsQuery, useAddProductMutation, useDeleteProductMutation} = itemApi;
+
