ee3eaa62a3fda37fb97ba9f51bc6885cf6e9ac6a
STyshchyk <svet.tyshchyk@gmail.com>
2023-03-30 14:25:59 +0300

    feat: add product router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
+import App from './Pages/App';
 import {createHashRouter, RouterProvider} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./store/store";
 
+const router = createHashRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <h1> Error loading</h1>
+    },
+    {
+        path: "product/:id",
+        element: <p>Hello</p>,
+        errorElement: <h1> Error card</h1>
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App/>
+            <RouterProvider router={router} base={"/"}/>
         </Provider>
     </React.StrictMode>
 );
